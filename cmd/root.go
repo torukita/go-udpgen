ad91644a2f855ed9ee3d220336862a2b78ef5808
torukita <torukita@outlook.com>
2017-02-02 20:19:44 +0900

    Changed command name

--- cmd/root.go
+++ cmd/root.go
@@ -20,6 +20,7 @@ var versionCmd = &cobra.Command{
 	},
 }
 
+/*
 var RootCmd = &cobra.Command{
 	Use:   "go-udpgen",
 	Short: "go-udpgen golang",
@@ -28,10 +29,10 @@ var RootCmd = &cobra.Command{
 		fmt.Println("command root")
 	},
 }
+*/
 
-
-var arootCmd = &cobra.Command{
-	Use:   "hoge",
+var RootCmd = &cobra.Command{
+	Use:   "go-udpgen",
 	Short: "udp generator for golang",
 	Long:  `UDP packets generate from cmd or web`,
 	Run: func(cmd *cobra.Command, args []string) {
@@ -57,28 +58,28 @@ var arootCmd = &cobra.Command{
 }
 
 func init() {
-	RootCmd.AddCommand(arootCmd)
+//	RootCmd.AddCommand(arootCmd)
 	RootCmd.AddCommand(versionCmd)
 
-	arootCmd.Flags().String("src-eth", "00:00:00:00:00:01", "Source mac address")
-	arootCmd.Flags().String("dst-eth", "00:00:00:00:00:02", "Dest mac address")
-	arootCmd.Flags().String("src-ip", "10.0.40.1", "Source IP address")
-	arootCmd.Flags().String("dst-ip", "10.0.40.2", "Dest IP address")
-	arootCmd.Flags().String("src-port", "9999", "UDP source port")
-	arootCmd.Flags().String("dst-port", "9999", "UDP dest port")
-	arootCmd.Flags().String("device", "eth0", "Device name")
-	arootCmd.Flags().Uint64("time", 0, "seconds which keeps sending packtes")
-	arootCmd.Flags().Uint64("count", 1, "The number of packets to be send")
+	RootCmd.Flags().String("src-eth", "00:00:00:00:00:01", "Source mac address")
+	RootCmd.Flags().String("dst-eth", "00:00:00:00:00:02", "Dest mac address")
+	RootCmd.Flags().String("src-ip", "10.0.40.1", "Source IP address")
+	RootCmd.Flags().String("dst-ip", "10.0.40.2", "Dest IP address")
+	RootCmd.Flags().String("src-port", "9999", "UDP source port")
+	RootCmd.Flags().String("dst-port", "9999", "UDP dest port")
+	RootCmd.Flags().String("device", "eth0", "Device name")
+	RootCmd.Flags().Uint64("time", 0, "seconds which keeps sending packtes")
+	RootCmd.Flags().Uint64("count", 1, "The number of packets to be send")
 	
 
-	viper.BindPFlag("root.src-eth", arootCmd.Flags().Lookup("src-eth"))
-	viper.BindPFlag("root.dst-eth", arootCmd.Flags().Lookup("dst-eth"))
-	viper.BindPFlag("root.src-ip", arootCmd.Flags().Lookup("src-ip"))
-	viper.BindPFlag("root.dst-ip", arootCmd.Flags().Lookup("dst-ip"))
-	viper.BindPFlag("root.src-port", arootCmd.Flags().Lookup("src-port"))
-	viper.BindPFlag("root.dst-port", arootCmd.Flags().Lookup("dst-port"))
-	viper.BindPFlag("root.device", arootCmd.Flags().Lookup("device"))
-	viper.BindPFlag("root.time", arootCmd.Flags().Lookup("time"))
-	viper.BindPFlag("root.count", arootCmd.Flags().Lookup("count"))
+	viper.BindPFlag("root.src-eth", RootCmd.Flags().Lookup("src-eth"))
+	viper.BindPFlag("root.dst-eth", RootCmd.Flags().Lookup("dst-eth"))
+	viper.BindPFlag("root.src-ip", RootCmd.Flags().Lookup("src-ip"))
+	viper.BindPFlag("root.dst-ip", RootCmd.Flags().Lookup("dst-ip"))
+	viper.BindPFlag("root.src-port", RootCmd.Flags().Lookup("src-port"))
+	viper.BindPFlag("root.dst-port", RootCmd.Flags().Lookup("dst-port"))
+	viper.BindPFlag("root.device", RootCmd.Flags().Lookup("device"))
+	viper.BindPFlag("root.time", RootCmd.Flags().Lookup("time"))
+	viper.BindPFlag("root.count", RootCmd.Flags().Lookup("count"))
 
 }
--- main.go
+++ main.go
@@ -11,35 +11,3 @@ func main() {
 		os.Exit(-1)
 	}
 }
-
-/*
-import (
-	"github.com/torukita/go-udpgen/pkt"
-	"github.com/torukita/go-udpgen/pcap"
-	"log"
-	"os"
-	"strconv"
-)
-
-func main() {
-	nic := os.Args[1]
-	num, _ := strconv.Atoi(os.Args[2])
-	handle, err := pcap.Open(nic)
-	if err != nil {
-			log.Fatal(err)
-	}
-	defer handle.Close()
-
-	eth := pkt.NewEthernet("11:22:33:44:55:66", "88:77:66:55:44:33")
-	ip := pkt.NewIPv4("10.10.0.10", "1.2.3.4")
-	udp := pkt.NewUDP(8000, 5000)
-	packet, err := pkt.UDPPacket(eth, ip, udp)
-	if err != nil {
-		log.Fatal(err)
-	}
-	for i := 0; i < num; i++ {
-		pcap.Send(handle, packet)
-	}
-	log.Println("Done Send")
-}
-*/
