3fe1092648d9a61207b5e2f1174c166b8ba2bb1c
torukita <torukita@outlook.com>
2017-02-09 16:59:39 +0900

    Change parameter of viper

--- README.md
+++ README.md
@@ -3,17 +3,9 @@
 This is just for sending udp packet to make load.
 Now under developing....
 
-## Plan
+## Required
 
-* execute from cmdline
-* execute from web
-* portable (include html file in memory)
-* send type
-  * single
-  * goroutine
-  * multi cpu
-  * etc..
-* only private address
+ * pcap library
 
 ## Usage (CLI mode)
 
--- cmd/root.go
+++ cmd/root.go
@@ -3,15 +3,9 @@ package cmd
 import (
 	"fmt"
 	"github.com/spf13/cobra"
-	"github.com/spf13/viper"
-	"github.com/torukita/go-udpgen/api"
-	"log"
-	"strconv"
-	"time"
-	"os"
 )
 
-var version = "v0.0.5"
+var version = "v0.0.6"
 
 var versionCmd = &cobra.Command{
 	Use:   "version",
@@ -19,46 +13,6 @@ var versionCmd = &cobra.Command{
 	Long:  `go-udpgen application version`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println(version)
-	},
-}
-
-var sendCmd = &cobra.Command{
-	Use:   "send [Interface Name]",
-	Short: "Send UDP packets",
-	Long:  `go-udpgen send can be used to send UDP packates from CLI`,
-	Example: `$ go-udpgen send eth0 --dst-ip 10.10.10.10`,
-	PreRun: func(cmd *cobra.Command, args []string) {
-		if len(args) != 1 {
-			cmd.Help()
-			os.Exit(-1)
-		}
-	},
-	Run: func(cmd *cobra.Command, args []string) {
-		devicename := args[0]
-		config := api.NewConfig()
-		config.Device = devicename
-		config.SrcEth = viper.GetString("src-eth")
-		config.DstEth = viper.GetString("dst-eth")
-		config.SrcIP = viper.GetString("src-ip")
-		config.DstIP = viper.GetString("dst-ip")		
-		udp_src, _ := strconv.Atoi(viper.GetString("src-port"))
-		udp_dst, _ := strconv.Atoi(viper.GetString("dst-port"))
-		config.SrcPort = uint16(udp_src)
-		config.DstPort = uint16(udp_dst)
-		config.Second = time.Duration(viper.GetInt64("time")) * time.Second
-		config.Count = uint64(viper.GetInt64("count"))
-		if viper.GetInt("concurrency") != 0 {
-			config.Concurrency = viper.GetInt("concurrency")
-		}
-		if viper.GetInt("size") != 64 {
-			config.Size = viper.GetInt("size")
-		}
-
-		err := config.ExecFromCLI()
-		if err != nil {
-			log.Fatal(err)
-		}
-		log.Println("Done Send")
 	},
 }
 
@@ -73,36 +27,4 @@ var RootCmd = &cobra.Command{
 
 func init() {
 	RootCmd.AddCommand(versionCmd)
-	RootCmd.AddCommand(sendCmd)
-
-	sendCmd.Flags().String("src-eth", "00:00:00:00:00:01", "Source mac address")
-	viper.BindPFlag("src-eth", sendCmd.Flags().Lookup("src-eth"))
-
-	sendCmd.Flags().String("dst-eth", "00:00:00:00:00:02", "Dest mac address")
-	viper.BindPFlag("dst-eth", sendCmd.Flags().Lookup("dst-eth"))
-
-	sendCmd.Flags().String("src-ip", "10.0.40.1", "Source IP address")
-	viper.BindPFlag("src-ip", sendCmd.Flags().Lookup("src-ip"))
-
-	sendCmd.Flags().String("dst-ip", "10.0.40.2", "Dest IP address")
-	viper.BindPFlag("dst-ip", sendCmd.Flags().Lookup("dst-ip"))
-
-	sendCmd.Flags().String("src-port", "9999", "UDP source port")
-	viper.BindPFlag("src-port", sendCmd.Flags().Lookup("src-port"))
-
-	sendCmd.Flags().String("dst-port", "9999", "UDP dest port")
-	viper.BindPFlag("dst-port", sendCmd.Flags().Lookup("dst-port"))
-
-	sendCmd.Flags().Uint64("time", 0, "seconds which keeps sending packtes")
-	viper.BindPFlag("time", sendCmd.Flags().Lookup("time"))
-
-	sendCmd.Flags().Uint64("count", 1, "The number of packets to be send")
-	viper.BindPFlag("count", sendCmd.Flags().Lookup("count"))	
-
-	sendCmd.Flags().Int("concurrency", 0, "The number of goroutines to use")
-	viper.BindPFlag("concurrency", sendCmd.Flags().Lookup("concurrency"))
-
-	sendCmd.Flags().Int("size", 64, "The length of ethernet frame (size >=64)")
-	viper.BindPFlag("size", sendCmd.Flags().Lookup("size"))
-
 }
